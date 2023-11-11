458f34583521b26552948b0de0663e1a6fa02463
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-11-11 23:38:44 +0530

    UI changes by Ganesh

--- farmoptiai/src/app/page.js
+++ farmoptiai/src/app/page.js
@@ -4,6 +4,10 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.body}>
+        <div> 1 </div>
+        <div> 2 </div>
+      </div>
     </main>
   )
 }
